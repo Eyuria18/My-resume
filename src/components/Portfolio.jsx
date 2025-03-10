5de93176e9b5c33e41f40738498dc660efdf2445
Draven Carter <draven6616@gmail.com>
2025-03-10 14:17:59 -0500

    readded portfolio and aboutme

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,11 @@
+function Portfolio() {
+    return (
+      <section>
+        <h2>Portfolio</h2>
+        <p>Here are some of my projects.</p>
+      </section>
+    );
+  }
+  
+  export default Portfolio;
+  
